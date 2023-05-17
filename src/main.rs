2c78abb5926d0ae9e9709c14796d45c93142b7a1
TheWisePigeon <zozozozeph@gmail.com>
2023-05-17 23:10:45 +0000

    add utils module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+pub mod utils;
+
+use std::env;
+use utils::init;
+
+fn main()-> () {
+    match env::args().nth(1) {
+        Some(command)=>{
+            match command.as_str() {
+                "help"=>{
+                    println!("Elsa is a dotfile manager");
+                },
+                "init"=>{
+                    init("thewisepigeon/dotfiles")
+                },
+                _=>{
+                    println!("Unrecognized command");
+                }
+            }
+        },
+        None=>{
+            println!("Elsa");
+        }
+    }
 }
